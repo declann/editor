9c9477dd86aab5d98dad1b42eb7c65be44275eca
Declan Naughton <declan.naughton2@mail.dcu.ie>
2021-11-19 14:50:37 +0000

    vendor.sh hacked for windows

--- scripts/vendor.sh
+++ scripts/vendor.sh
@@ -14,7 +14,7 @@ if [ ! -d "$DATA" ]; then
   mkdir $DATA
 fi
 
-eval rsync -r "$CWD/node_modules/vega-datasets/data/*" $DATA
+eval rsync -r "./node_modules/vega-datasets/data/*" $DATA
 
 echo "Copy examples to '$SPEC'."
 
@@ -32,9 +32,26 @@ curl https://github.com/vega/vega-lite/archive/v$VEGA_LITE_VERSION.tar.gz -L -o 
 tar xzf vega.tar.gz vega-$VEGA_VERSION/docs
 tar xzf vl.tar.gz vega-lite-$VEGA_LITE_VERSION/examples vega-lite-$VEGA_LITE_VERSION/site/_data
 popd
+# tar x of vl fails due to symlinks but if motered on would be fine?
 
-eval rsync -r "/tmp/vega-$VEGA_VERSION/docs/examples/*.vg.json" "$SPEC/vega"
-eval rsync -r "/tmp/vega-lite-$VEGA_LITE_VERSION/examples/specs/*.vl.json" "$SPEC/vega-lite/"
+# https://superuser.com/questions/584146/why-is-this-archive-failing-to-create-symlinks
+#tar xzf vl.tar.gz 2> /tmp/tar-stdxtr.t
+#(export DIR=; echo \#\!/bin/sh -e; cat /tmp/tar-stderr.txt | grep 'Cannot create symlink to' | sed -e 's7^tar: \.\([^:]\+\): Cannot create symlink to .\([^:]\+\).*$7\1:\27' -e "s7^\(.\+\)/\([^:]\+\):\(.\+\).\$7cd $DIR\1 \&\& cp \3 \2;7")
+
+
+# + manually created public/spec/vega[-lite] dirs
+
+eval cp -r "/tmp/vega-$VEGA_VERSION/docs/examples/*.vg.json" "$SPEC/vega"
+#eval cp -r "/tmp/vega-lite-$VEGA_LITE_VERSION/examples/specs/*.vl.json" "$SPEC/vega-lite/"
 
 cp "/tmp/vega-lite-$VEGA_LITE_VERSION/site/_data/examples.json" "$SPEC/vega-lite/index.json"
 cp "/tmp/vega-$VEGA_VERSION/docs/_data/examples.json" "$SPEC/vega/index.json"
+
+
+#cp -R /tmp/vega-lite-5.1.1/examples//specs/*.vl.json ./public/spec/vega-lite
+
+#cp /tmp/vega-lite-5.1.1/site/_data/examples.json ./public/spec/vega-lite/index.json
+
+cp /tmp/vega-lite-5.1.1/site/_data/examples.json ./public/spec/vega-lite/index.json
+
+cp /tmp/vega-5.21.0/docs/_data/examples.json ./public/spec/vega/index.json
