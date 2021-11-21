a8cbcc0d96e080a1a54a0b4d30b41853dc04860f
Declan Naughton <declan.naughton2@mail.dcu.ie>
2021-11-21 13:47:55 +0000

    up

--- src/components/dn.tsx
+++ src/components/dn.tsx
@@ -1,13 +1,13 @@
 export default [
-  {function: 'A', value: 5},
-  {function: 'B', value: 11},
-  {function: 'C', value: 10},
-  {function: 'D', value: 10},
-  {function: 'E', value: 100},
-  {function: 'F', value: 5},
-  {function: 'G', value: 13}, // hello
-  {function: 'H', value: 11},
-  {function: 'I', value: 52},
+  { function: "A", value: 5 },
+  { function: "B", value: 11 },
+  { function: "C", value: 10 },
+  { function: "D", value: 10 },
+  { function: "E", value: 100 },
+  { function: "F", value: 10 },
+  { function: "G", value: 13 }, // hello
+  { function: "H", value: 15 },
+  { function: "I", value: 52 }
 ];
 
 // now this can be setup to import from a json file that calculang build&run process rights to, and done...
