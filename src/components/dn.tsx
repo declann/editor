bae4ff63c853188094a777e315a92b86b4ae8a58
Declan Naughton <declan.naughton2@mail.dcu.ie>
2021-11-22 12:53:21 +0000

    +c

--- src/components/dn.tsx
+++ src/components/dn.tsx
@@ -1,13 +1,16 @@
 export default [
-  { function: "A", value: 5 },
-  { function: "B", value: 11 },
-  { function: "C", value: 10 },
-  { function: "D", value: 10 },
-  { function: "E", value: 100 },
-  { function: "F", value: 10 },
-  { function: "G", value: 131 }, // hello
-  { function: "H", value: 15 },
-  { function: "I", value: 52 }
+  {function: 'A', value: 5},
+  {function: 'B', value: 11},
+  {function: 'C', value: 10},
+  {function: 'D', value: 10},
+  {function: 'E', value: 100},
+  {function: 'F', value: 10},
+  {function: 'G', value: 131}, // hello
+  {function: 'H', value: 15},
+  {function: 'I', value: 52},
 ];
 
 // now this can be setup to import from a json file that calculang build&run process rights to, and done...
+
+// this or app-shell should link in data from run-bounce.json
+// or use a calculang.json type config?
