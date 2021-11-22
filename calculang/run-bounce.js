224f55b76f92f2102b358b114fd2ca26aa520dc4
Declan Naughton <declan.naughton2@mail.dcu.ie>
2021-11-22 12:50:21 +0000

    push calculang bounce model setup

---
+++ calculang/run-bounce.js
@@ -0,0 +1,36 @@
+var bounce = require('./bounce.js');
+
+const t_end = 20;
+const dampener_in = 0.96;
+const dx_in = 3;
+
+function projection() {
+  let projection_prime = [];
+  [...Array(t_end).keys()].forEach((t_in) => {
+    projection_prime.push({t_in, dx_in, dampener_in, function: 'x', value: Math.round(bounce.x({t_in, dx_in}))});
+    projection_prime.push({
+      t_in,
+      dx_in /* keep? */,
+      dampener_in,
+      function: 'y',
+      value: Math.round(bounce.y({t_in, dampener_in})),
+    });
+    projection_prime.push({
+      t_in,
+      dx_in,
+      dampener_in,
+      function: 'dy',
+      value: Math.round(bounce.dy({t_in, dampener_in})),
+    });
+    projection_prime.push({
+      t_in,
+      dx_in,
+      dampener_in,
+      function: 'compressed',
+      value: bounce.compressed({t_in, dampener_in}) ? 1 : 0,
+    });
+  });
+  return projection_prime;
+}
+
+console.log(JSON.stringify(projection()));
